5e6312912226680247aa4707e85c7f27589f25d8
ID24-CMR <idrice.tsafouet@yahoo.com>
2024-08-14 07:16:30 +0200

    linking the project to firebase
    this is my first time
    i am just trying to see what it will give

---
+++ src/main/java/org/id24/mns237/Mns237Application.java
@@ -0,0 +1,14 @@
+package org.id24.mns237;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mns237Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Mns237Application.class, args);
+  }
+
+}
+
